e6faeab7704395d9b3179e0f42a7cf6a129c0f61
Jeswin <jeswinw07@gmail.com>
2025-01-27 20:41:28 +0530

    auth and token generation

--- AgentAuth/AgentAuth.java
+++ AgentAuth/AgentAuth.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
+
+import com.me.db.PgConnector;
+import com.me.generator.AgentTokenGenerator;
+
 public class AgentAuth {
 
     
     public static void main(String args[]) {
-        System.out.println("zChoose: \n1.create customer \n2.create group \n3.create user \n4.exit");
         Scanner sc = new Scanner(System.in);
-        String choise = sc.nextLine();
 
+        boolean x = true;
+        while (x) {
+            System.out.println("zChoose: \n1.create  \n4.exit");
+            String choise = sc.nextLine();
         switch (choise) {
             case "1":
-                System.out.println("");
+                System.out.println("create ");
+                PgConnector.insert(new AgentTokenGenerator());
                 break;
         
             default:
                 break;
         }
+    }
         sc.close();
 
     }
--- AgentAuth/com/me/authenticate/AuthenticateFilter.java
+++
@@ -1,5 +0,0 @@
-package com.me.authenticate;
-
-public class AuthenticateFilter {
-    
-}
